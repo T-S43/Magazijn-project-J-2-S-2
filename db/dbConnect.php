2a31cb049bed45f389bd493504306b962d03a553
Turan <330506@student.mboutrecht.nl>
2022-02-22 14:46:16 +0100

    Added and edited scripts
    Added: new MBO logo and logo script
    
    Edit: dbconnect now shows a error message

--- db/dbConnect.php
+++ db/dbConnect.php
@@ -4,4 +4,8 @@
   define("PASSWORD", "");
   define("DBNAME", "magazijn_Mbo_Utrecht");
   $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+
+  if (!$conn ) {
+    die("connection_failed".mysqli_connect_error());
+}
 ?>
