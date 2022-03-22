b9657a0e935298dd942e47110c035ea086107069
QEUN <332275@student.mboutrecht.nl>
2022-03-22 16:50:49 +0100

    added the files to start with OOP
    there are several deretories made and files to start and use OOP and make a read system for the warehouse items

--- db/dbConnect.php
+++ db/dbConnect.php
@@ -3,6 +3,7 @@
   define("USERNAME", "root");
   define("PASSWORD", "");
   define("DBNAME", "magazijn_Mbo_Utrecht");
+  define('APPROOT', dirname(dirname(__FILE__)));
   $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 
   if (!$conn ) {
---
+++ model/Warehouse.php
@@ -0,0 +1,7 @@
+<?php
+class Warehouse{
+    public function getWarehouse(){
+        
+
+    }
+}
