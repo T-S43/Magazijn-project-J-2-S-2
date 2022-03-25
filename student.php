e0b4515bb66900a8237b78488adf9bd50606c132
Turan <330506@student.mboutrecht.nl>
2022-03-25 09:55:42 +0100

    Added the student order page
    Made the student order page where the students can choose the item they want to order.
    
    Added a href that sends them to a form

--- student.php
+++ student.php
@@ -5,6 +5,46 @@ if (! ($userRole == "4") ) {
     header("Refresh: 0; ./index.php");
 }
 // choose between 1: super user,    2: warehouse-admin,     3: financial admin,     4: student
+
+$sql = "SELECT * FROM `warehouse`";
+$result = mysqli_query($conn, $sql);
+
+// record is used for the table top row.
+$record = "";
+ 
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Naam</th>";
+    echo "<th>Hoeveelheid</th>";
+    echo "<th>Aanvragen?</th>";
+    echo "</tr>";
+// records goes through all records that come through the database and prints them out if the requirement is correct.
+    $records = "";
+
+while ($record = mysqli_fetch_assoc($result)) {
+    if($record["available"] == 1) {
+        $records .= "<tr><td>" .
+            $record["Name"] . "</td><td>" .
+            $record["Amount"] . "</td>" .
+                "<td>
+                    <a href='./student_aanvragen.php?id=" . $record ["id"] . "'>X</a>
+                </td>;
+            </tr>";
+        }
+    }
 ?>
-<h2>Welkom <?php echo($_SESSION["firstname"])?> <?php echo($_SESSION["lastname"])?></h2>
-<h2>User roll = <?php echo ($_SESSION["UserRoll"])?></h2>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student</title>
+</head>
+<body>
+
+
+<p><?php echo $records?></p>
+</body>
+</html>
