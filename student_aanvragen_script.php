a56956fe8afd6959a154fdb3bd25eb98a2d0e719
Turan <330506@student.mboutrecht.nl>
2022-03-29 14:39:46 +0200

    Added student bestelling page
    Users can see request that they made

--- student_aanvragen_script.php
+++ student_aanvragen_script.php
@@ -1,9 +1,6 @@
 <?php
 include('./functions/userRoleSystem.php');
-    // $email = $_SESSION['email'];
-
-// $user = "SELECT `id` FROM `users` WHERE `email` = $email";
-
+// Grabbing data not only from the page but also from the session
     $userId = $_SESSION['id'];
     $id = $_POST['id'];
     $amount = $_POST["amount"];
@@ -12,27 +9,6 @@ include('./functions/userRoleSystem.php');
 
 $sql = "INSERT INTO `request` (`id`, `userId`, `warehouseId`, `amount`, `message`, `location`) VALUES (NULL, '$userId','$id','$amount','$message','$location')";
 
-
-echo $sql;
-echo "<br><br> $id <br><br> $amount <br><br> $message <br><br> $location<br><br>";
-echo "$userId";
-var_dump($_SESSION);
-
 mysqli_query($conn, $sql);
-
-// header("Location: ./student.php");
+header("Location: ./student.php");
 ?>
-
-<!-- $firstname = sanitize($_POST["firstname"]);
-    $infix = sanitize($_POST["infix"]);
-    $lastname = sanitize($_POST["lastname"]);
-    $email = sanitize($_POST["email"]);
-    $password = sanitize($_POST["password"]);
-
-    //schrijft de gegevens naar de tabel
-    $sql = "INSERT INTO `users` (`id`, `firstname`, `infix`, `lastname`, `email`) VALUES (NULL, '$firstname', '$infix', '$lastname', '$email');";
-
-    // Functie is de query sql te laten verbinden via conn naar de database.
-    mysqli_query($conn, $sql);
-
-    header("Location: ./read.php"); -->
