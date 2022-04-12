b11eea13d08de576b255b820308c873218a42047
LAPTOP-0TH2B7ID\martin <martintrap1@gmail.com>
2022-04-12 14:32:40 +0200

    changed SQL to remove collate
    collate was causing import errors. removing this fixed it.

--- Database/magazijn_mbo_utrecht.sql
+++ Database/magazijn_mbo_utrecht.sql
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `lend` (
   PRIMARY KEY (`lendID`),
   KEY `WarehouseID` (`warehouseID`),
   KEY `UserID` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
